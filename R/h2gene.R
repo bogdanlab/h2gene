6309eb1c5cb72ed00bf06a3b7c1a7446a90c7a2f
Kangcheng Hou <kangchenghou@gmail.com>
2022-01-05 13:41:29 -0800

    more checks of arguments

--- R/h2gene.R
+++ R/h2gene.R
@@ -1,32 +1,37 @@
-h2gene <- function(susie_fit, ld, annot, n_sample=500) {
+h2gene <- function(susie_fit, ld, annot, n_sample = 500) {
   # susie_fit: susie fit object
   # ld: LD matrix used as input to fit susie object
   # annot: #SNP x #annot binary matrix denoting the
   #   membership for each SNP in each annotation
   # n_sample: number of samples to simulate
 
-  stopifnot(length(susie_fit$pip) == nrow(annot))
-  posterior_samples <- susie_get_posterior_samples(susie_fit, n_sample)
+  if (length(susie_fit$pip) != nrow(annot)) {
+    stop("Number of SNPs to construct susie fit must be equal to the number of rows in annotation")
+  }
+  if (!all(annot %in% c(F, T))) {
+    stop("Annotation matrix must be binary, only `T` and `F` is allowed")
+  }
+  posterior_samples <-
+    susie_get_posterior_samples(susie_fit, n_sample)
 
   # form place-holder for h2 and n_causal
   hsq_estimates <- matrix(0., nrow = n_sample, ncol = ncol(annot))
-  ncausal_estimates <- matrix(0., nrow = n_sample, ncol = ncol(annot))
+  ncausal_estimates <-
+    matrix(0., nrow = n_sample, ncol = ncol(annot))
 
   # for each sample / each annotation, calculate h2 / causal_num
   for (i in 1:n_sample) {
     this_b <- posterior_samples$b[, i]
     # for every annotation
-    for (a_i in 1 : ncol(annot)) {
+    for (a_i in 1:ncol(annot)) {
       mask <- annot[, a_i]
-      hsq_estimates[i, a_i] <- this_b[mask] %*% ld[mask, mask] %*% this_b[mask]
+      hsq_estimates[i, a_i] <-
+        this_b[mask] %*% ld[mask, mask] %*% this_b[mask]
       ncausal_estimates[i, a_i] <- sum(this_b[mask] != 0)
     }
   }
   colnames(hsq_estimates) <- colnames(annot)
   colnames(ncausal_estimates) <- colnames(ncausal_estimates)
-  return(list(
-    hsq = hsq_estimates,
-    ncausal = ncausal_estimates
-  ))
+  return(list(hsq = hsq_estimates,
+              ncausal = ncausal_estimates))
 }
-
